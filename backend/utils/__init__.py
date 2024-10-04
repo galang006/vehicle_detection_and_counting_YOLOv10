01960081206bc583943f57753a3037c18c924c59
Galang Satriamahesa Putra Dewa <galangsatriamahesa.pd@gmail.com>
2024-10-04 20:48:14 +0700

    update init

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -2,4 +2,5 @@ from .tracking import save_track, calculate_speed
 from .view_transformer import ViewTransformer
 from .display import draw_line, draw_text_with_background, display_vehicle_count, generate_vehicle_count_text
 from .save_to_csv import save_data_to_csv
-from .config import *
+from .config import video_url, LINES_COUNT, LINES_RECTANGLE, SOURCE, TARGET, directories, VEHICLE_CLASSES, vehicle_count, vehicle_track, TARGET_WIDTH, TARGET_HEIGHT, last_saved_minute
+from .detection import vehicle_detection
