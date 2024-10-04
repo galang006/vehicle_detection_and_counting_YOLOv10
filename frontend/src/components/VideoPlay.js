5b6458881a59e0b8ce0f284560560b0662fb6567
Galang Satriamahesa Putra Dewa <galangsatriamahesa.pd@gmail.com>
2024-10-04 20:52:44 +0700

    update videoplay

--- frontend/src/components/VideoPlay.js
+++ frontend/src/components/VideoPlay.js
@@ -1,22 +1,44 @@
+// import React, { useEffect, useRef } from 'react';
+// import Hls from 'hls.js'; 
+
+// const VideoPlay = () => {
+//   const videoRef = useRef(null);
+
+//   useEffect(() => {
+//     if (Hls.isSupported()) {
+//       const hls = new Hls();
+//       const video = videoRef.current;
+//       hls.loadSource('http://localhost:5000/start_stream');
+//       hls.attachMedia(video);
+//     }
+//   }, []);
+
+//   return (
+//     <div style={{ padding: '20px' }}>
+//       <div style={{ marginBottom: '20px' }}>
+//         <video ref={videoRef} controls style={{ width: '50%', height: 'auto' }} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default VideoPlay;
+
 import React, { useEffect, useRef } from 'react';
-import Hls from 'hls.js'; 
 
 const VideoPlay = () => {
   const videoRef = useRef(null);
 
   useEffect(() => {
-    if (Hls.isSupported()) {
-      const hls = new Hls();
-      const video = videoRef.current;
-      hls.loadSource('https://cctvjss.jogjakota.go.id/atcs/ATCS_Simpang_Amongrogo_View_Timur.stream/chunklist_w14842540.m3u8');
-      hls.attachMedia(video);
+    if (videoRef.current) {
+      videoRef.current.src = 'http://localhost:5000/start_stream';
     }
   }, []);
 
   return (
     <div style={{ padding: '20px' }}>
       <div style={{ marginBottom: '20px' }}>
-        <video ref={videoRef} controls style={{ width: '50%', height: 'auto' }} />
+        <img ref={videoRef} alt="Video stream" style={{ width: '50%', height: 'auto' }} />
       </div>
     </div>
   );
