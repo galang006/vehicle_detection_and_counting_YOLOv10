2e75b89ed24734f712ff0f03ef4d70fdb1e02b14
Galang Satriamahesa Putra Dewa <galangsatriamahesa.pd@gmail.com>
2024-10-09 15:41:37 +0700

    update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,3 @@
-from collections import defaultdict, deque
-
-import cv2
-import numpy as np
-from ultralytics import YOLO
-import supervision as sv
-import pandas as pd
-from datetime import datetime
-import os
 from utils import *
 
 if __name__ == "__main__":
