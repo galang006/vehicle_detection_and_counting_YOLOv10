7635be80dcf15341448967f4e78ec371b3bf3d86
Galang Satriamahesa Putra Dewa <galangsatriamahesa.pd@gmail.com>
2024-10-10 12:22:28 +0700

    Update : app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,39 @@ import './App.css';
 import VehicleLineChart from './components/VehicleLineChart';
 import VehicleDataTable from './components/VehicleTableChart';
 import VideoPlay from './components/VideoPlay';
+import VehicleDetail from './components/VehicleDetail';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Vehicle Dashboard</h1>
+    <Router>
+      <div className="App">
+        <h1>Vehicle Dashboard</h1>
 
-      <div style={{ marginBottom: '50px' }}>
-        <VideoPlay />
+        <Routes>
+          {/* Route for the dashboard */}
+          <Route
+            path="/"
+            element={
+              <>
+                <div style={{ marginBottom: '50px' }}>
+                  <VideoPlay />
+                </div>
+                <div style={{ marginBottom: '50px' }}>
+                  <VehicleLineChart />
+                </div>
+                <div>
+                  <VehicleDataTable />
+                </div>
+              </>
+            }
+          />
+
+          {/* Route for vehicle detail page */}
+          <Route path="/vehicle/:trackId/:className" element={<VehicleDetail />} />
+        </Routes>
       </div>
-      <div style={{ marginBottom: '50px' }}>
-        <VehicleLineChart />
-      </div>
-      <div >
-        <VehicleDataTable />
-      </div>
-    </div>
+    </Router>
   );
 }
 
