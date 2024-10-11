205e1f4a7da8615a4cb3400aefba2193c73cd4de
Galang Satriamahesa Putra Dewa <galangsatriamahesa.pd@gmail.com>
2024-10-11 13:08:53 +0700

    Update

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,7 @@
-from flask import Flask, jsonify, request, Response, send_from_directory,  send_file, make_response
-import subprocess
-import os
+from flask import Flask, jsonify, request, send_from_directory,  send_file, make_response
 import pandas as pd
 from flask_cors import CORS
-from collections import defaultdict, deque
 
-import cv2
-import numpy as np
-from ultralytics import YOLO
-import supervision as sv
-from datetime import datetime
 from utils import *
 
 app = Flask(__name__)
@@ -26,35 +18,18 @@ def serve_segment(segment_number):
     segment_filename = f'video/segment_{segment_number:03d}.ts'
     return send_file(segment_filename)
 
-
-# @app.route('/vehicle_track/<track_id>', methods=['GET'])
-# def get_vehicle_by_track_id(track_id):
-#     data_file = 'data/vehicle_track.csv'  # Update this to your CSV file path
-#     vehicle_data = pd.read_csv(data_file)
-#     # Filter the vehicle data based on Track ID
-#     track_id_int = int(track_id)  # Convert to integer for filtering
-    
-#     # Filter the vehicle data based on Track ID
-#     filtered_data = vehicle_data[vehicle_data['Track ID'] == track_id_int]
-
-#     if not filtered_data.empty:
-#         return jsonify(filtered_data.to_dict(orient='records'))
-#     else:
-#         return jsonify({'message': 'No data found for the given Track ID'}), 404
-
 @app.route('/images/<path:filename>')
 def serve_image(filename):
     return send_from_directory('data/images', filename)
 
 @app.route('/vehicle_track', methods=['GET'])
 def filter_vehicle_data():
-    track_id = request.args.get('track_id', type=int)  # Get Track ID as integer
-    class_name = request.args.get('class_name', type=str)  # Get Class Name
+    track_id = request.args.get('track_id', type=int) 
+    class_name = request.args.get('class_name', type=str) 
 
-    data_file = 'data/vehicle_track.csv'  # Update this to your CSV file path
+    data_file = 'data/vehicle_track.csv' 
     vehicle_data = pd.read_csv(data_file)
 
-    # Start filtering based on provided parameters
     filtered_data = vehicle_data
 
     if track_id is not None:
@@ -63,7 +38,6 @@ def filter_vehicle_data():
     if class_name:
         filtered_data = filtered_data[filtered_data['Class Name'] == class_name]
 
-    # Construct the image URLs 
     image_urls = []
     for _, row in filtered_data.iterrows():
         direction_folder = row['Direction'].split('_')[0]
@@ -83,60 +57,6 @@ def filter_vehicle_data():
         return jsonify(filtered_data_list)
     else:
         return jsonify({'message': 'No data found for the given filters'}), 404
-
-# @app.route('/vehicle_track', methods=['GET'])
-# def filter_vehicle_data():
-#     track_id = request.args.get('track_id', type=int)  # Get Track ID as integer
-#     class_name = request.args.get('class_name', type=str)  # Get Class Name
-#     timestamp = request.args.get('timestamp', type=str)  # Get Timestamp
-#     direction = request.args.get('direction', type=str)  # Get Direction
-
-#     data_file = 'data/vehicle_track.csv'  # Update this to your CSV file path
-#     vehicle_data = pd.read_csv(data_file)
-
-#     # Start filtering based on provided parameters
-#     filtered_data = vehicle_data
-
-#     if track_id is not None:
-#         filtered_data = filtered_data[filtered_data['Track ID'] == track_id]
-
-#     if class_name:
-#         filtered_data = filtered_data[filtered_data['Class Name'] == class_name]
-
-#     if timestamp:
-#         filtered_data = filtered_data[filtered_data['Timestamp'] == timestamp]
-
-#     if direction:
-#         # Extract the word before the underscore for filtering
-#         direction_prefix = direction.split('_')[0]
-#         filtered_data = filtered_data[filtered_data['Direction'].str.startswith(direction_prefix)]
-# # Construct the image URLs for the filtered data
-#     base_image_path = 'data/images/'  # Base path for the images
-#     image_urls = []
-
-#     for _, row in filtered_data.iterrows():
-#         # Construct the directory and image filename
-#         direction_folder = row['Direction'].split('_')[0]  # Get the direction prefix
-#         image_name = f"{row['Class Name']}_{row['Track ID']}_{row['Timestamp']}.jpg"  # Assuming jpg format
-#         image_path = f"{base_image_path}{direction_folder}/{image_name}"  # Complete path
-        
-#         # Add the image URL to the row data
-#         image_urls.append({
-#             'Track ID': row['Track ID'],
-#             'Image URL': image_path  # Add the image URL
-#         })
-
-#     if not filtered_data.empty:
-#         # Convert filtered data to a list of dictionaries
-#         filtered_data_list = filtered_data.to_dict(orient='records')
-        
-#         # Add image URLs to the filtered data
-#         for i, item in enumerate(filtered_data_list):
-#             item['Image URL'] = image_urls[i]['Image URL'] if i < len(image_urls) else None
-        
-#         return jsonify(filtered_data_list)
-#     else:
-#         return jsonify({'message': 'No data found for the given filters'}), 404
 
 @app.route('/vehicle_count', methods=['GET'])
 def get_vehicle_count():
--- frontend/src/components/VehicleTableChart.js
+++ frontend/src/components/VehicleTableChart.js
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router-dom';
 
 const VehicleDataTable = () => {
   const [vehicleData, setVehicleData] = useState([]);
-  // const [selectedData, setSelectedData] = useState(null);
   const navigate = useNavigate(); 
 
   useEffect(() => {
@@ -14,19 +13,8 @@ const VehicleDataTable = () => {
       .catch(error => console.error('Error fetching data:', error));
   }, []);
 
-  // Function to handle row click
   const handleRowClick = (trackId, className) => {
     navigate(`/vehicle/${trackId}/${className}`);
-    // console.log('Clicked Track ID:', trackId);
-    // console.log('Clicked Class Name:', className);
-
-    // fetch(`http://127.0.0.1:5000/vehicle_track?track_id=${trackId}&class_name=${className}`)
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     console.log('Fetched data:', data);  // Log the data returned from the API
-    //     setSelectedData(data);  // You can set this to state or process it further
-    //   })
-    //   .catch(error => console.error('Error fetching specific data:', error));
   };
 
   return (
